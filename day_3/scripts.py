572f3554a7ddc46662a818a1de9a22d88ad02216
Bobb <uncle.bobb.dev@gmail.com>
2023-02-09 12:27:36 -0700

    day_3 scripts - finished

--- day_3/scripts.py
+++ day_3/scripts.py
@@ -1,3 +1,5 @@
+__name__ = 'day_3_scripts'
+
 import geometry.geometry as geo
 
 '''
@@ -8,6 +10,7 @@ I guess most of the examples in operators.py are 'scripts,' but this will have t
 
 # Write a script that prompts the user to enter base and height
 # of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
+
 
 def get_tri_area():
     try:
@@ -23,6 +26,7 @@ def get_tri_area():
 
 # Write a script that prompts the user to enter side a, side b, and side
 # c of the triangle. Calculate the perimeter of the triangle (perimeter = a + b + c).
+
 
 def get_tri_perim():
     try:
@@ -62,10 +66,14 @@ def secs_in_years():
 
         return 60 * 60 * 24 * 365.24 * years
 
+    except ValueError:
+        print('Please enter an integer or decimal number')
+        secs_in_years()
 
 # 23
 
 # Write a Python script that displays the following table
+
 
 '''
 1 1 1 1 1
@@ -74,3 +82,43 @@ def secs_in_years():
 4 1 4 16 64
 5 1 5 25 125
 '''
+
+# column zero is indices, columns1-4 are index^column(n-1), eg column 1 is index^0, or all 1s, column 2 is index^1, etc.
+# this is a rather naive method of creating this table
+
+
+def display_table():
+    rows, cols = (5, 5)
+    table = [[0 for i in range(cols)] for j in range(rows)]
+
+    # Make index col
+    for i in range(rows):
+        table[i][0] = i + 1
+
+    # index^0 col
+    for i in range(rows):
+        table[i][1] = table[i][0] ** 0
+
+    # index^1 col; just index col in all ZFC set theory situations
+    for i in range(rows):
+        table[i][2] = table[i][0]
+
+    # index^2 col
+    for i in range(rows):
+        table[i][3] = table[i][0] ** 2
+
+    # index^3 col
+    for i in range(rows):
+        table[i][4] = table[i][0] ** 3
+
+    # print formatted table
+    output = ""
+
+    for row in table:
+        num_count = 0
+        for num in row:
+            output = output + str(num) + " "
+            num_count += 1
+            if num_count == 5:
+                output = output + "\n"
+    print(output)
